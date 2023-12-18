22a507d1b67de09e2e7562499d47428de75b96f4
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-12-18 17:37:14 +1300

    updated lambda fn implementation

--- lambda_function.py
+++ lambda_function.py
@@ -1,37 +1,25 @@
 import boto3
 import logging
 
-ec2 = boto3.client('ec2')
+ec2 = boto3.resource('ec2')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# sns_client = boto3.client('sns')
-volumes = ec2.describe_volumes()
-
 def lambda_handler(event, context):
-    unused_volumes = []
-    for vol in volumes['Volumes']:
-        if len(vol['Attachments']) == 0:
-            vol1 = ("-----Unused Volume ID = {}------".format(vol['VolumeId']))
-            unused_volumes.append(vol1)
-    
-    #email
-    # sns_client.publish(
-    #     TopicArn='<SNS Topic ARN>',
-    #     Subject='Warning - Unused Volume List',
-    #     Message=str(unused_volumes)
-    # )
+    logger.info("identifying unattached volumes...")
 
-    print("EBS vols to delete:")
-    for vol in unused_volumes:
-        print(vol)
+    vol_filter = [
+        {'Name':'tag:AutoDelete', 'Values':['True']},
+        {'Name': 'status','Values': ['available']}
+    ]
 
-    return {
-        "statusCode": 200,
-        "isBase64Encoded": False,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": {"unused_volumes": unused_volumes}
-    }
+    volumes = [v for v in ec2.volumes.filter(Filters=vol_filter)]
+
+    logger.info(f"volumes unattached: {volumes}")
+
+    for vol in volumes:
+        logger.info(f"deleting volume: {vol.id}")
+        vol.delete()
+
+    logger.info("unattached volumes deleted")
